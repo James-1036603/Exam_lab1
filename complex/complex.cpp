b1dcfbd01b46f5eeb1c6a0f8d474904c753f42de
Unknown <1036603@students.wits.ac.za>
2017-11-05 17:29:50 +0200

    Exercise 3.2

--- complex/complex.cpp
+++ complex/complex.cpp
@@ -3,13 +3,12 @@
 
 #include <iostream>
 #include <complex> // required for the complex class
-
 using namespace std;
-
+typedef std::complex<float> complexFloat;
 int main()
 {
-    complex<float> num1{ 2.0, 2.0 };  // using C++11 uniform initialisation syntax
-    complex<float> num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
+    complexFloat num1{ 2.0, 2.0 };  // using C++11 uniform initialisation syntax
+    complexFloat num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
 
     auto answer = num1 * num2; // using C++11 auto keyword,
                                // answer is of type: complex<float>
@@ -17,8 +16,7 @@ int main()
     cout << "The answer is: " << answer << endl;
     cout << "Or in a more familiar form: " << answer.real() << " + " << answer.imag() << "j" << endl;
 
-    // answer++; This code will not work, as an operator has not been defined to increment the floating type. Complex is a template class which does not work like a single variable type
-	
+    // answer++; This code will not work, as an operator has not been defined to increment the floating type. Complex is a template class which does not work like a single variable type	
 
     return 0;
 }
