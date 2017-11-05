448911c9189803fef8164cec628da37e78616f10
Unknown <1036603@students.wits.ac.za>
2017-11-05 17:23:15 +0200

    Exercise 3.1

---
+++ complex/complex.cpp
@@ -0,0 +1,24 @@
+// complex.cpp
+// Multiplying complex numbers
+
+#include <iostream>
+#include <complex> // required for the complex class
+
+using namespace std;
+
+int main()
+{
+    complex<float> num1{ 2.0, 2.0 };  // using C++11 uniform initialisation syntax
+    complex<float> num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
+
+    auto answer = num1 * num2; // using C++11 auto keyword,
+                               // answer is of type: complex<float>
+
+    cout << "The answer is: " << answer << endl;
+    cout << "Or in a more familiar form: " << answer.real() << " + " << answer.imag() << "j" << endl;
+
+    // answer++; This code will not work, as an operator has not been defined to increment the floating type. Complex is a template class which does not work like a single variable type
+	
+
+    return 0;
+}
